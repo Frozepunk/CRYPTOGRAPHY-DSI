bcc97a925b4be3196df51ae874a48cf7d2fd391b
Abdul Ahanaafez <118284998+AHANAAF@users.noreply.github.com>
2024-11-18 19:05:09 +0530

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,29 +1,23 @@
-import random
-from Crypto.Util.number import getPrime, inverse
-
-def generate_rsa_keys(key_size=1024):
-    e = 65537  # Public exponent
-    p = getPrime(key_size // 2)
-    q = getPrime(key_size // 2)
-    n = p * q
-    phi = (p - 1) * (q - 1)
-    d = inverse(e, phi)
-    return ((e, n), (d, n))
-
-def rsa_encrypt(plaintext, public_key):
-    e, n = public_key
-    plaintext_int = [pow(ord(char), e, n) for char in plaintext]
-    return plaintext_int
-
-def rsa_decrypt(ciphertext, private_key):
-    d, n = private_key
-    decrypted_chars = [chr(pow(char, d, n)) for char in ciphertext]
-    return ''.join(decrypted_chars)
-
-# Example usage
-public_key, private_key = generate_rsa_keys(512)
-plaintext = input("Enter the plaintext for RSA: ")
-encrypted = rsa_encrypt(plaintext, public_key)
-print("Encrypted Text:", encrypted)
-decrypted = rsa_decrypt(encrypted, private_key)
-print("Decrypted Text:", decrypted)
+import math
+from sympy import mod_inverse
+p = int(input("Enter the value of p (prime): "))
+q = int(input("Enter the value of q (prime): "))
+n = p * q
+phi = (p - 1) * (q - 1)
+print("n =", n)
+print("phi(n) =", phi)
+e = int(input("Enter the value of e (1 < e < phi(n)): "))
+while e >= phi or math.gcd(e, phi) != 1:
+    print("Invalid e. Ensure 1 < e < phi (n)")
+    e = int(input("Enter a valid value for e: "))
+print("e =", e)
+d = mod_inverse(e, phi)
+print("d =", d)
+print(f'Public key: ({e}, {n})')
+print(f'Private key: ({d}, {n})')
+msg = int(input("Enter the value of the message (as an integer): "))
+print(f'Original message: {msg}')
+C = pow(msg, e, n)
+print(f'Encrypted message: {C}')
+M = pow(C, d, n)
+print(f'Decrypted message: {M}')
